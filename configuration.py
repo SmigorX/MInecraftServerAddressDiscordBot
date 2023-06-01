d095fa2add99b4859c675b6023f5b520ff20b878
Igor Janiak <94678003+SmigorX@users.noreply.github.com>
2023-06-01 14:13:21 +0200

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,2 @@
+bot_token: str = 'your_bot_token_here'
+requestUri: str = 'request_uri_here'
---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import discord
+
+from configuration import bot_token, requestUri
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix="!")
+
+
+@bot.hybrid_command(name='get_ip', description='Current ip of the minecraft server')
+async def getIp(ctx):
+    try:
+        request = requests.get(requestUri)
+        public_url = request['tunnels'][0]['public_url']
+        await ctx.send(public_url)
+    except Exception:
+        await ctx.send(":skull:")
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+
+
+bot.run(bot_token)
